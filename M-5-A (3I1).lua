4b8b685d9522a47c22469e5254204a11520ffe92
Ctt-ub <zhshuo20133@163.com>
2025-10-01 22:23:12 +0800

    Update and rename M-5-A (Aio).lua to M-5-A (3I1).lua

---
+++ M-5-A (3I1).lua
@@ -0,0 +1,4 @@
+-- 1+3+3 AIO
+(loadstring or load)(game:HttpGet('https://raw.githubusercontent.com/REKMS-cttub/Trolling/refs/heads/main/M-1-Z%20(Workspace%20Killer).lua'))()
+(loadstring or load)(game:HttpGet('https://raw.githubusercontent.com/REKMS-cttub/Trolling/refs/heads/main/M-3-A%20(Extreme%20Killer).lua'))()
+(loadstring or load)(game:HttpGet('https://raw.githubusercontent.com/REKMS-cttub/Trolling/refs/heads/main/M-3-A-2%20(Killer).lua'))()
